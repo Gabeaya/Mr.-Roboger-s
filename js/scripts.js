b49bfafe04499c240c0462d39e1d2f7c1d627dff
Gabriel Ayala <gabeayala100@gmail.com>
2021-07-05 14:06:26 -0700

    Change direction of where response goes from robogerReturn to 'UL'.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 
 function beepBoop(number) {
-  if (number.trim().length === 0) {
+  if (number.trim().length === 0) { //If inputted number, after being stripped of whitespace, is equal to zero it will return zero
 
   return 0;
 }
@@ -11,8 +11,8 @@ function beepBoop(number) {
   const neighbor = ("Won't you be my neighbor?");
   let robotArray = [];
 
-  for(let i = 0; i <= number; i++) {
-    let string = i.toString();
+  for(let i = 0; i <= number; i++) { //We start our loop at an index of zero, we will iterate this loop through each if,else if, or else statement until the index is equal to the inputted number
+    let string = i.toString(); // All iterations will become strings
     if (string.includes(3)) {
       robotArray.push(neighbor);
     } else if (string.includes(2)) {
@@ -24,18 +24,17 @@ function beepBoop(number) {
     }
   }
   return robotArray;
-  
 
 }
 
 $(document).ready(function() {
   $('form#userInput').submit(function(event) {
     event.preventDefault();
-    const numberInput = $("input#number").val();
-    let response = beepBoop(numberInput).join(" ");
+    const numberInput = $("input#number").val(); // Variable assigned to user input
+    let response = beepBoop(numberInput).join(" "); // Variable assigned to the result of our joined function that passes the user inputs as an argument
 
-    $("#robogerReturn").text(response);
-    $("#robogerReturn").show();
+    $("ul").text(response); // The translation of our jQuery result into our html
+    $("#robogerReturn").show(); //Show our once hidden roboger result field
     
   })
 })
