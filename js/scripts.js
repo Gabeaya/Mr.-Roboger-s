5ada05638b3cc97ed03bd32df2c829ccfc45ee56
Gabriel Ayala <gabeayala100@gmail.com>
2021-07-05 11:06:36 -0700

    Create a function to display results.

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,14 @@
+const beep = ["Beep!"];
+
+const boop = ["Boop!"];
+
+const neighbor = ["Won't you be my neighbor?"];
 function beepBoop(number) {
   if (number.trim().length === 0) {
-    
+
   return 0;
 }
   let robotArray = [];
-  const beep = ["Beep!"];
-
-  const boop = ["Boop!"];
-
-  const neighbor = ["Won't you be my neighbor?"];
 
   for(let i = 0; i <= number; i++) {
     string = i.toString();
@@ -25,4 +25,21 @@ function beepBoop(number) {
   }
   return robotArray.join(" ");
 }
-beepBoop("3", "4");
+beepBoop("0");
+
+$(document).ready(function() {
+  $('form#userInput').submit(function(event) {
+    event.preventDefault();
+    let response = beepBoop(numberInput);
+    response.forEach(function(element) {
+      if(element === neighbor) {
+        $("#robogerReturn").prepend("<li>Won't you be my neighbor?</li>")
+      } else if (element === boop) {
+        $("#robogerReturn").append("<li>Boop!</li>")
+      } else if (element === beep) {
+        $("#robogerReturn").append("<li>Beep!</li>")
+      }    
+    })
+    $("#robogerReturn").show();
+  })
+})
