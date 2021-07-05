82eda33440bb64800cd91113dc102a90574fbed5
Gabriel Ayala <gabeayala100@gmail.com>
2021-07-05 09:11:11 -0700

    Create a function that takes user input and returns an array of all numbers leading to input starting at 0.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,16 @@
+
+function arrayForInput(number) {
+  let newArray = [];
+  let start = 0;
+  while (start <= number) {
+    newArray.push(start)
+    start++;
+  }
+    return newArray;
+}
 function beepBoop(number) {
   if (number.trim().length === 0) {
+    
   return 0;
 }
   const beep = ["Beep!"];
@@ -7,25 +18,28 @@ function beepBoop(number) {
   const boop = ["Boop!"];
 
   const phraseForThree = ["Won't you be my neighbor?"];
+  let userInput = parseInt(number);
 
-  let userInput = number.split("")
-  userInput = userInput.map(Number);
-  
   for(let i = 0; i < userInput.length; i++) {
-    if(userInput[0] === 1) {
-      if(userInput[1] !== 3) {
-      return beep;
-      } else if(userInput[1] === 3) {
-        return phraseForThree;
-      };
+    if (userInput[0] === 3) {
+      if(userInput !== 2) {
+      return phraseForThree;
+      } else if (userInput ===2)
+      return phraseForThree;
     } else if (userInput[0] === 2) {
         if(userInput[1] !== 1) {
         return boop;
         } else if(userInput[1] === 1) {
         return boop;
       };
-    } else if (userInput[0] === 3) {
-    return phraseForThree;
+    } else if(userInput[0] === 1) {
+      if(userInput[1] !== 3) {
+      return beep;
+      } else if(userInput[1] === 3) {
+        return phraseForThree;
+      };
+    } else {
+      return userInput
     };
   }
 }
